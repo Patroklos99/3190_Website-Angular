a455c816a1f68d1eadd84bd630c8300a126f6cb1
kovalenko.elizaveta <jd191048@ens.uqam.ca>
2021-10-18 01:13:28 -0400

    rajoutement de tout nos fichier fait jusq'a la, le version finale pour le moment

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function getParamParNom(nom, url = window.location.href) {
+    nom = nom.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + nom + '(=([^&#]*)|&|#|$)'),
+        resultat = regex.exec(url);
+    if (!resultat) return null;
+    if (!resultat[2]) return '';
+    return decodeURIComponent(resultat[2].replace(/\+/g, ' '));
+}
