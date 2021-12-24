86136aed5a8cdb46d0c9738eed83a3e66e8f4120
kovalenko.elizaveta <jd191048@ens.uqam.ca>
2021-12-23 23:45:59 -0500

    rajout des instruction au fichier README

--- clec-app/src/app/app.component.ts
+++ clec-app/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 //import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
